66896c3c6c3d47541034ead3c69352f97d8c15c3
KajaGrzybowska <kajagrzybowska@outlook.com>
2017-06-12 16:56:42 +0200

    użyłam lenght do podzielenia stringa

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
 // scripts.js
 
-var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period";
+var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.";
 var dinosaur ="Triceratops";
-var dinosaurUpperCased = dinosaur.toUpperCase();
-var replacement = text.replace("Velociraptor", dinosaurUpperCased);
+var dinosaurUpperCased = dinosaur.toUpperCase(); //duże litery do zmiennej "dinosaur" 
+var replacement = text.replace("Velociraptor", dinosaurUpperCased); // zmienna "dinosaur" duzymi literami zastępuje słowo "velociraptor" w stringu "text"
 
-alert(replacement);
-console.log(replacement);
+alert(replacement); 
+console.log(replacement); //zmieniony string 
 
-console.log(replacement.length / 2);
+console.log(replacement.length / 2); // wyświetli 144
 
-alert(replacement.substr(0, 72));
-console.log(replacement.substr(0, 72));
+alert(replacement.substr(0, replacement.length / 2)); 
+console.log(replacement.substr(0, replacement.length / 2)); // (samo replacement.length / 2) - pokazuje połowę, ale drugą
